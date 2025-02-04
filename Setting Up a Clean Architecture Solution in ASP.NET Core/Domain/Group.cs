8d4fdbbbb4afab2bdfb043fcfc22bfa4a6188d80
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-02-04 22:21:49 +0500

    Creation of Domain Layer

---
+++ Setting Up a Clean Architecture Solution in ASP.NET Core/Domain/Group.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+
+namespace Domain
+{
+    public class Group : BaseEntity
+    {
+        public string GroupName { get; set; } = string.Empty;
+        public List<SubGroup>? SubGroups { get; set; }
+    }
+}
