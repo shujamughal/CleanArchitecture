aeb882a994e61e51c1afa738b88706e0a87ecd9e
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-02-05 23:18:12 +0500

    updated

---
+++ CleanArchitecture/CleanArchitecture.Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain
+{
+    public class Order : BaseEntity
+    {
+        public string OrderNumber { get; set; } = string.Empty;
+        public DateTime OrderDate { get; set; }
+
+        // Foreign key and navigation property: Each order belongs to a customer.
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+    }
+
+}
