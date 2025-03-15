2aabcf8e1a4f3f49325961c5b61596e4db85eb1e
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-03-15 23:29:15 +0500

    Clean Architecture  Series

---
+++ Lecture4/Clean/Application/MappingProfiles/CustomerProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain;
+
+namespace Application.MappingProfiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<CustomerDTO, Customer>().ReverseMap();
+        }
+    }
+}
