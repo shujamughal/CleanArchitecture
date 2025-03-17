3b7921c1fb5791319ab4c533fb4b70cc9e0c4c33
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-03-18 01:28:44 +0500

    Clean Architecture Series

---
+++ Lecture17/Clean/Application/Features/Customer/Queries/GetCustomerDetails/GetCustomerDetailsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Exceptions;
+using Application.Interfaces.Persistence;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Customer.Queries.GetCustomerDetails
+{
+    public class GetCustomerDetailsQueryHandler : IRequestHandler<GetCustomerDetailsQuery, CustomerDetailsDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetCustomerDetailsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<CustomerDetailsDTO> Handle(GetCustomerDetailsQuery request, CancellationToken cancellationToken)
+        {
+            // Fetch single record
+            var customerDetails = await _unitOfWork.Customers.GetFirstOrDefaultAsync(c => c.Id == request.Id);
+
+            if (customerDetails == null)
+            {
+                // Throw NotFoundException if the Customer is not found
+                throw new NotFoundException(nameof(Customer), request.Id);
+            }
+
+            // Map data to DTO
+            var data = _mapper.Map<CustomerDetailsDTO>(customerDetails);
+
+            return data;
+        }
+    }
+
+}
---
+++ Lecture17/Clean/Application/Features/Order/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Exceptions;
+using Application.Interfaces.Persistence;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Order.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateOrderCommandHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            // Fluent Validation
+            var validator = new CreateOrderCommandValidator(_unitOfWork);
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                throw new BadRequestException("Invalid Order data.", validationResult);
+            }
+
+            // Map the request to an Order entity
+            var orderToCreate = _mapper.Map<Domain.Order>(request);
+
+            // Add the Order to the database
+            await _unitOfWork.Orders.AddAsync(orderToCreate);
+
+            // Save changes
+            await _unitOfWork.Save();
+
+            // Return the ID of the newly created Order
+            return orderToCreate.Id;
+        }
+    }
+}
