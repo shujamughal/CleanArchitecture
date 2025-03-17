d2cbb577f2c5851ec5eaf4ddd622b91fc2085928
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-03-17 22:33:03 +0500

    Clean Architecture Series

---
+++ Lecture12/Clean/Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Results;
+
+namespace Application.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public List<string> ValidationErrors { get; set; }
+
+        public BadRequestException(string message) : base(message)
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        public BadRequestException(string message, ValidationResult validationResult) : base(message)
+        {
+            ValidationErrors = new List<string>();
+            foreach (var error in validationResult.Errors)
+            {
+                ValidationErrors.Add(error.ErrorMessage);
+            }
+        }
+    }
+
+
+}
---
+++ Lecture12/Clean/Application/Features/Order/Queries/GetOrderDetails/GetOrderDetailsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application.Features.Order.Queries.GetOrderDetails
+{
+    public record GetOrderDetailsQuery(int Id) : IRequest<OrderDetailsDTO>;
+}
