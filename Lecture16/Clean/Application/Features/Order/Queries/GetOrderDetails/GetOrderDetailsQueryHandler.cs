2da90a4d8c6f58699084f9f6df5a08cd0732043e
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-03-18 00:17:50 +0500

    Clean Architecture Serries

---
+++ Lecture16/Clean/Application/Features/Order/Queries/GetOrderDetails/GetOrderDetailsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Exceptions;
+using Application.Interfaces.Persistence;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Order.Queries.GetOrderDetails
+{
+    public class GetOrderDetailsQueryHandler : IRequestHandler<GetOrderDetailsQuery, OrderDetailsDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetOrderDetailsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderDetailsDTO> Handle(GetOrderDetailsQuery request, CancellationToken cancellationToken)
+        {
+            // Fetch single record
+            var orderDetails = await _unitOfWork.Orders.GetFirstOrDefaultAsync(o => o.Id == request.Id);
+
+            if (orderDetails == null)
+            {
+                // Throw NotFoundException if the Order is not found
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            // Map data to DTO
+            var data = _mapper.Map<OrderDetailsDTO>(orderDetails);
+
+            return data;
+        }
+    }
+}
