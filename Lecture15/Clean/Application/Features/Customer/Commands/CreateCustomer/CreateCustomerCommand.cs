d61676aa8afe32c466e5818f59252f8d4651b9c6
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-03-17 23:57:25 +0500

    Clean Architecture Series

---
+++ Lecture15/Clean/Application/Features/Customer/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Features.Customer.Commands.CreateCustomer
+{
+    public class CreateCustomerCommand : IRequest<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ Lecture15/Clean/Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register third-party services here
+
+            return services;
+        }
+    }
+
+}
