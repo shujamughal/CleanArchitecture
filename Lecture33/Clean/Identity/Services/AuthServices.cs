6d7c87b88b297265f633f5949999b1d758de94a4
Muhammad Ahmad <158601056+ahmadrajpoot062@users.noreply.github.com>
2025-03-19 01:26:31 +0500

    Clean Architecture Series

---
+++ Lecture33/Clean/Identity/Services/AuthServices.cs
@@ -0,0 +1,21 @@
+﻿using Application.Identity;
+using Application.Models.Identity;
+using System.Threading.Tasks;
+
+namespace Identity.Services
+{
+    public class AuthServices : IAuthService
+    {
+        public Task<AuthResponse> Login(AuthRequest request)
+        {
+            // TODO: Implement login logic
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            // TODO: Implement registration logic
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Lecture33/Clean/WebApp/Models/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
